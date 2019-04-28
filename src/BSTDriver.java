8c1d8230404d6e0b7dfbf5a4db50ef811f30edba
Schlitzohr <wemx836@gmail.com>
2019-04-28 13:35:11 -0700

    Driver Frame

--- src/BSTDriver.java
+++ src/BSTDriver.java
@@ -1,6 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class BSTDriver {
     public static void main(String[] args) {
         BST myBst = new BST();
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
 
+        char option = 'Z';
+        int num = 0;
+        treeStats(myBst);
+    }
+    public static char menu() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("(A)dd   (B)alanced?   (P)arent (C)hildren   (N)ew Tree   (T)ree display  (R)andomize   (Q)uit");
+        char option = in.next().charAt(0);
+        option = Character.toUpperCase(option);
+        return option;
+    }
+    public static void treeStats(BST tree) {
+        System.out.println("\nWelcome to the Jungle");
+        System.out.println("-Tree Stats- Root:"+tree.getRoot());
+        System.out.println("");
     }
 }
